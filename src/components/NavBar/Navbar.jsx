4afa79417de55922f267fd96b54d640707f5f843
sebnem <sebnemgormus@gmail.com>
2025-04-18 15:18:31 +0300

    Major updates: components, auth, and UI improvements

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import searchIcon from '../../assets/search_icon.svg'
 import bellIcon from '../../assets/bell_icon.svg'
 import profileImg from '../../assets/profile_img.png'
 import dropdownIcon from '../../assets/caret_icon.svg'
+import { logout } from '../../firebase'
 
 
-function Navbar() {
+const Navbar = () => {
+
+  const navRef = useRef();
+
+  useEffect(()=>{
+    window.addEventListener('scroll', ()=>{
+      if(window.scrollY >= 80) {
+        navRef.current.classList.add('nav-dark')
+      } else {
+        navRef.current.classList.remove('nav-dark')
+      }
+    })
+  },[])
+
   return (
-    <div className='navbar'>
+    <div ref={navRef} className='navbar'>
       <div className="navbar-left">
         <img src={logo} alt="netflix logo" />
         <ul>
@@ -29,7 +43,7 @@ function Navbar() {
           <img src={profileImg} className='profile' alt="profile icon" />
           <img src={dropdownIcon} alt="search icon" />
           <div className='dropdown'>
-            <p>Sign Out of Netflix</p>
+            <p onClick={()=>{logout()}}>Sign Out of Netflix</p>
           </div>
         </div>
       </div>
--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
-import './TitleCards.css'
+import React, { useEffect, useRef, useState } from "react";
+import "./TitleCards.css";
+import { Link } from "react-router-dom";
 
-function TitleCards() {
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+    },
+  };
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+
+    const cardList = cardsRef.current;
+
+    cardList.addEventListener("wheel", handleWheel);
+
+    return () => {
+      cardList.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
   return (
-    <div className='titlecards'>TitleCards</div>
-  )
-}
+    <div className="title-cards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default TitleCards
+export default TitleCards;
